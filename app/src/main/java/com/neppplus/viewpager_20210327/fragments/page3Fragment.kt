7a133a4e9b9de60b0bf93e0b373c33d25a48669d
장혜진 <aslk3366zz@naver.com>
2021-03-27 13:41:38 +0900

    프래그먼트 3개 작성

---
+++ app/src/main/java/com/neppplus/viewpager_20210327/fragments/page3Fragment.kt
@@ -0,0 +1,20 @@
+package com.neppplus.viewpager_20210327.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.viewpager_20210327.R
+
+class page3Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_page3, container, false)
+    }
+
+}
